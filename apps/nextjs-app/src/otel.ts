f1b656a895488be940764429c69019b1ca9ad1c2
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2026-04-22 15:28:38 -0400

    disable http and undici instrumentation for next-js which duplicates nextjs tracing

--- apps/nextjs-app/src/otel.ts
+++ apps/nextjs-app/src/otel.ts
@@ -6,11 +6,10 @@ export const { logger } = initializeOtel({
     : "nextjs-app",
   instrumentations: {
     "@opentelemetry/instrumentation-http": {
-      ignoreIncomingRequestHook: (request) => {
-        // Ignore all Next.js internal routes (paths starting with /_)
-        const url = request.url ?? "";
-        return url.startsWith("/_") || url.startsWith("/favicon");
-      },
+      enabled: false,
+    },
+    "@opentelemetry/instrumentation-undici": {
+      enabled: false,
     },
   },
 });
